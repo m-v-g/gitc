6c0a15e72b1d46f7b762e8b025fa1579fb803651
Vasil Mkrtchyan <m-v-g@mail.ru>
2022-06-03 17:04:07 +0400

    folder

--- das_14/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-   //stexcenq nor obekt iran veragrenq poxancvac obekti glux@
-            for(int i = 0; i < x.listsQuantity; ++i)  //qayleri qanak@ = e x-i mej exac uzelneri qanakin
-            {
-                push(temp -> value);                
